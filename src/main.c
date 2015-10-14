34c259cab379d3ceb456a578d8cd78035ea4cefb
fabiogomesvieira <fabiogomesvieira@hotmail.com>
2015-10-14 12:12:34 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,41 +5,35 @@
 #include <string.h>
 #include "../lib/mongoose.h"
 #include "../lib/sqlite3.h"
+#include "../lib/index.h"
 
 #define Tbuffer 150000
+
 
 static int ev_handler(struct mg_connection *conn, enum mg_event ev) {
     char buffer[Tbuffer];
     buffer[0] = '\x0';
-    FILE *arq;
-    char ch;
-    int x;
 
     switch (ev) {
         case MG_AUTH:
             return MG_MORE;
 
         case MG_REQUEST:
-            mg_send_header(conn, "Content-Type", "text/html");
-            
-            char index[] ="web/index.html";
-            arq = fopen(index, "r");
-            if (arq == NULL) {
-                printf("Error 404 - Página não encontrada\n");
-            } else {
-                for (x = 0; (ch = fgetc(arq)) != EOF && x < Tbuffer; x++) {
-                    buffer[x] = ch;
-                }
-               fclose(arq);
-            }
-
-            mg_printf_data(conn, buffer);
-
-            return MG_TRUE;
+	
+			mg_send_header(conn, "Content-Type", "text/html");
+		
+			if(home(buffer, sizeof(buffer)) == 0){
+				return MG_FALSE;
+			}
+		
+			mg_printf_data(conn, buffer);
+			return MG_TRUE;	
+			
         default:
             return MG_FALSE;
     }
 }
+
 
 int main(void) {
 
@@ -49,7 +43,7 @@ int main(void) {
     mg_set_option(server, "document_root", ".");
     mg_set_option(server, "listening_port", "8080");
 
-    // Requisição do servidor. Ctrl-C Para terminar o programa
+    // requisiçao do servidor. Ctrl-C Para terminar o programa
     printf("Starting on port %s\n", mg_get_option(server, "listening_port"));
     for (;;) {
         mg_poll_server(server, 1000);
@@ -60,5 +54,3 @@ int main(void) {
 
     return 0;
 }
-
-
